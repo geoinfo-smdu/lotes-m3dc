09943c84512bdbd10ae71e6947a7d4485230a404
Fernando Gomes <feromes@gmail.com>
2020-05-07 17:49:58 -0300

    Pequenos ajustes

--- lotes_fiscais.py
+++ lotes_fiscais.py
@@ -5,32 +5,34 @@ from pyproj import Proj, transform, Transformer
 from ipyleaflet import Map, WMSLayer, basemaps, Marker, WidgetControl, Polygon as LeafLetPolygon
 from ipywidgets import Label
 
-url_wms = 'http://wms.geosampa.prefeitura.sp.gov.br/geoserver/geoportal/wms'
-wms = WebMapService(url_wms, version='1.3.0')
-coords_wgs84 = [-23.545567989349365, -46.6351979970932]
-center = coords_wgs84
+class LotesFiscais:
+    def __init__(self):
+        self.url_wms = 'http://wms.geosampa.prefeitura.sp.gov.br/geoserver/geoportal/wms'
+        self.wms = WebMapService(self.url_wms, version='1.3.0')
+        self.coords_wgs84 = [-23.545567989349365, -46.6351979970932]
+        self.center = self.coords_wgs84
 
-mapa_base = WMSLayer(
-    url = url_wms,
-    layers = 'MapaBase_Politico',
-    format = 'image/png',
-    transparent = True,
-    attribution = 'GeoSampa'
-)
+        self.mapa_base = WMSLayer(
+            url = self.url_wms,
+            layers = 'MapaBase_Politico',
+            format = 'image/png',
+            transparent = True,
+            attribution = 'GeoSampa'
+        )
 
-lotes = WMSLayer(
-    url = 'http://wms.geosampa.prefeitura.sp.gov.br/geoserver/geoportal/wms',
-    layers = 'lote_cidadao',
-    format = 'image/png',
-    transparent = True,
-    attribution = 'GeoSampa'
-)
+        self.lotes = WMSLayer(
+            url = 'http://wms.geosampa.prefeitura.sp.gov.br/geoserver/geoportal/wms',
+            layers = 'lote_cidadao',
+            format = 'image/png',
+            transparent = True,
+            attribution = 'GeoSampa'
+        )
 
-transformer = Transformer.from_crs(31983, 4326)
+        self.transformer = Transformer.from_crs(31983, 4326)
 
-marker = Marker(location=center, draggable=True)
+        self.marker = Marker(location=self.center, draggable=True)
 
-shape = ''
+        self.shape = ''
 
 def geo2utm(coord_geo):
     in_proj  = Proj("EPSG:4326")
@@ -47,43 +49,48 @@ def get_polygon_coord(coordinate):
     c = coordinate
     s = 0.25
     lote = wms.getfeatureinfo(layers = ['lote_cidadao'],
-                          format = 'image/png',
-                          info_format = 'application/json',
-                          srs = 'EPSG:31983',
-                          bbox = (c[0] - s, c[1] - s, c[0] + s, c[1] + s),
-                          size = (2, 2), 
-                          xy = (1, 1))
+                        format = 'image/png',
+                        info_format = 'application/json',
+                        srs = 'EPSG:31983',
+                        bbox = (c[0] - s, c[1] - s, c[0] + s, c[1] + s),
+                        size = (2, 2), 
+                        xy = (1, 1))
     lote_json = json.load(lote)
     # print(lote_json)
     return lote_json['features'][0]['geometry']['coordinates'][0]
 
 def handle_click(*args, **kwargs):
+    print(**kwargs)
     lote_xy = geo2utm(marker.location)
     poligono_coord = get_polygon_coord(lote_xy)
 #     print(list(transformer.itransform(poligono_coord)))
 #     print(poligono_coord)
     poligono = LeafLetPolygon(locations=list(transformer.itransform(poligono_coord)), 
-                              color="green", 
-                              fill_color="green")
+                            color="green", 
+                            fill_color="green")
     m.add_layer(poligono)
     display(Polygon(poligono_coord))
 
 
-def _map():
-    m = Map(center=center, 
-            zoom=17,
-        scroll_wheel_zoom = True)
+class GeoSampaMap:
+        
+        def _map():
+            lf = LotesFiscais()
 
-    m.add_layer(mapa_base)
-    m.add_layer(lotes)
+            m = Map(center=lf.center, 
+                    zoom=17,
+                scroll_wheel_zoom = True)
 
-    m.add_layer(marker)
+            m.add_layer(lf.mapa_base)
+            m.add_layer(lf.lotes)
 
-    marker.on_click(handle_click)
+            m.add_layer(lf.marker)
 
-    return m
+            lf.marker.on_click(handle_click)
 
-m = _map()
+            return m
 
-def show_map():
-    return m
+        m = _map()
+
+        def show_map():
+            return m
